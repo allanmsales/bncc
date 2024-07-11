530f04f956d9dba791b13448787ffbf8a476ee73
allan <allanmsales@gmail.com>
2024-07-11 18:54:06 -0300

    feature: Observability frontend

--- controller/agent.py
+++ controller/agent.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Request, Form
 from fastapi.responses import HTMLResponse 
 from fastapi.templating import Jinja2Templates
+import json
 from use_case import rag
 from entity.history import History
 
@@ -24,3 +25,10 @@ def get_send_question(request: Request, question: str = Form(...)):
     history.add_to_history_user(question)
     content = rag.rag_pipeline(question, history)
     return templates.TemplateResponse("home.html", {"request": request, "content": content})
+
+
+@router.get("/observability", response_class=HTMLResponse)
+def front_observability(request: Request):
+    with open('observability.json', 'r') as file:
+        data = json.load(file)
+    return templates.TemplateResponse("observability.html", {"request": request, "content": data})
